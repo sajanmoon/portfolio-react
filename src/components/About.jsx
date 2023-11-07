0de2ada139c637b52ccf586be64b74d12e32222c
sajan moon <sajanmoon5@gmail.com>
2023-11-07 10:52:03 +0530

    added about section

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className=" w-full h-screen bg-[#0a192f] text-gray-400">
+      <div className=" flex flex-col justify-center items-center w-full h-full">
+        <div className=" max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className=" sm:text-right pb-8 pl-4">
+            <p className=" text-4xl font-bold  ">
+              About
+            </p>
+          </div>
+        </div>
+        <div className=" max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className=" sm:text-right text-4xl font-bold">
+            <p>Hi,I am Sajan, nice to meet you. Please take a look around</p>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              consequatur, reprehenderit eaque laboriosam provident maxime
+              necessitatibus unde, id quasi assumenda debitis minima, reiciendis
+              culpa illo sunt accusantium sed fugiat explicabo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,23 +46,38 @@ const Navbar = () => {
       <div className=" flex fixed flex-col top-[35%] left-0">
         <ul className="">
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
-            <a href="/" className=" flex justify-between items-center w-full ">
+            <a
+              href="https://www.linkedin.com/in/sajan-moon/"
+              target="_blank"
+              className=" flex justify-between items-center w-full "
+            >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-red-400">
-            <a href="/" className=" flex justify-between items-center w-full ">
+            <a
+              href="mailto:sajanmoon5@gmail.com"
+              className=" flex justify-between items-center w-full "
+            >
               Mail <AiOutlineMail size={30} />
             </a>
           </li>
 
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 text-black bg-white">
-            <a href="/" className=" flex justify-between items-center w-full ">
+            <a
+              href="https://github.com/sajanmoon"
+              target="_blank"
+              className=" flex justify-between items-center w-full "
+            >
               Github <BsGithub size={30} />
             </a>
           </li>
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-500">
-            <a href="/" className=" flex justify-between items-center w-full ">
+            <a
+              href="https://twitter.com/SajanMoon2"
+              target="_blank"
+              className=" flex justify-between items-center w-full "
+            >
               Twitter <RiTwitterXFill size={30} />
             </a>
           </li>
